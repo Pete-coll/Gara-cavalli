a34eae0f0043aa573eb81d205216cc9a0bcbb59b
osamu <osamu@PetePc>
2024-10-23 20:05:04 +0200

    Secondo Commit

---
+++ src/main/java/com/mycompany/garacavalli/GaraCavalli.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.garacavalli;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author osamu
+ */
+public class GaraCavalli {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // leggere diversi inpunt su console 
+         
+       
+        System.out.println("inserire lunghezza percorso");
+       
+       System.out.println("numero di cavalli");
+       int Ncavalli = scanner.nextInt();
+       
+       for(int i = 0; i < Ncavalli; i++){
+               
+           System.out.println("nome dei cavalli : " + (i+1));
+           
+               }
+       
+       System.out.println("gara iniziata");
+       
+      
+       
+    }
+    
+}
